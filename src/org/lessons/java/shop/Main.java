fecbe0be2c14639b4fdb1a25a462748b4943d073
ruzica-glavas <ruzi99@hotmail.it>
2025-05-05 16:47:41 +0200

    add: incapsulamento

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -2,24 +2,32 @@ package org.lessons.java.shop;
 
 public class Main {
     public static void main(String[] args) {
-        Prodotto videogioco = new Prodotto("The Last of us", "Gioco di avventura", 60.55f,22);
-        Prodotto console = new Prodotto("PlayStation 5", "Console creata da Sony", 512.50f, 22 );
+        Prodotto videogioco = new Prodotto("The Last of us", "Naughty Dog", 60.55f,22);
+        Prodotto console = new Prodotto("PlayStation 5", "Sony", 512.50f, 22 );
+
+        Prodotto tastiera = new Prodotto("Tastiera", "Logitech", 20.50f, 22 );
 
 
         
-        System.out.println("Codice random:" + videogioco.code);
+        System.out.println("Codice random:" + videogioco.getCode());
         //System.out.println("Nome prodotto:" + videogioco.nameProduct);
         System.out.println("Nome prodotto completo:" + videogioco.getFullName());
         //System.out.println("Descrizione prodotto:" + videogioco.description);
         System.out.println("Prezzo base prodotto:" + videogioco.getPrice() + " " + "euro");
-        System.out.println("Prezzo prodotto con iva:" + String.format("%.2f",videogioco.getPriceIva()) + " " + "euro");
+        System.out.println("Prezzo prodotto con iva:" + String.format("%.2f",videogioco.getIva()) + " " + "euro");
 
         
-        System.out.println("Codice random:" + console.code);
+        System.out.println("Codice random:" + console.getCode());
         //System.out.println("Nome prodotto:" +  console.nameProduct);
         System.out.println("Nome prodotto completo:" + console.getFullName());
         //System.out.println("Descrizione prodotto:" + console.description);
         System.out.println("Prezzo base prodotto:" + console.getPrice() + " " + "euro");
-        System.out.println("Prezzo prodotto con iva:" + String.format("%.2f", console.getPriceIva()) + " " + "euro");
+        System.out.println("Prezzo prodotto con iva:" + String.format("%.2f", console.getIva()) + " " + "euro");
+
+        System.out.println("Nome prodotto:" +  tastiera.getNameProduct());
+        System.out.println("Brand del prodotto:" + tastiera.getBrand());
+        System.out.println("Prezzo base prodotto:" + tastiera.getPrice() + " " + "euro");
+        System.out.println("Prezzo prodotto con iva:" + String.format("%.2f", tastiera.getIva()) + " " + "euro");
+       
     }
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -3,40 +3,76 @@ package org.lessons.java.shop;
 public class Prodotto{
     //Caratteristiche tipiche del prodotto (attributi)
 
-    public int code;
-    public String nameProduct;
-    public String description;
-    public float price;
-    public int iva;
+    private int code;                //Modifico il livello di accessibilità degli attributi
+    private String nameProduct;
+    //private String description;
+    private String brand;
+    private float price;
+    private int iva;
 
     //Capacità
 
-    public Prodotto(String nameProduct, String description, float price, int iva){
+    public Prodotto(String nameProduct,  /*String description*/ float price, int iva){
 
         this.code= getRandom(1,100);
         this.nameProduct= nameProduct;
-        this.description=description;
+        //this.description=description;
         this.price=price;
         this.iva=iva;
 
     }
 
-    public int getRandom(int min, int max){
+    public Prodotto(String nameProduct, String brand, float price, int iva){  //Secondo costruttore
+        this.nameProduct=nameProduct;
+        this.brand=brand;
+        this.price=price;
+        this.iva=iva;
+    }
+
+    private int getRandom(int min, int max){  //Cambio da public a private così da avere il codice solo in lettura
         int range = (max-min) + 1;
         int random = (int)((range*Math.random()+min));
         return random;
     }
 
+    public int getCode(){
+        return this.code;
+    }
 
+    public String getNameProduct(){
+        return this.nameProduct;
+    }
+
+    public void setNameProduct(String nameProduct){
+        this.nameProduct=nameProduct;
+    }
+
+    public String getBrand(){
+        return this.brand;
+    }
+
+    public void setBrand(String brand){
+        this.brand=brand;
+    }
+
+    
     public float getPrice(){
         return price;
     }
 
-    public float getPriceIva(){
+    public void setPrice(float price){
+        this.price=price;
+    }
+
+    public float getIva(){
         if(price !=0 && iva !=0){
             return price + (price*iva)/100;
         }
         return 0;
+    }
+
+    public void setIva(int iva){
+        this.iva=iva;
     }
 
     public String getFullName(){
